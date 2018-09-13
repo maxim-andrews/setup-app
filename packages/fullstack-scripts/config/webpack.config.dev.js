5f152089d2e5982329f6e0d660a1e5c687f168b9
Maxim Andrews <maximandrewz@users.noreply.github.com>
2018-09-13 22:06:18 +0100

    decrease url-loader embeded file limit to 1000

--- packages/fullstack-scripts/config/webpack.config.dev.js
+++ packages/fullstack-scripts/config/webpack.config.dev.js
@@ -158,7 +158,7 @@ module.exports = {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
             loader: require.resolve('url-loader'),
             options: {
-              limit: 10000,
+              limit: 1000,
               name: 'static/media/[name].[hash:8].[ext]',
             },
           },
