b717f3ad68215bdb06c8d201b97de31fa3452708
Maxim Andrews <maximandrews@users.noreply.github.com>
2019-09-29 17:52:51 -0700

    clean up code

--- packages/setup-app-templates/templates/react-redux-thunk-router/src/Store/ConfigureStore.csrOnly.js
+++ packages/setup-app-templates/templates/react-redux-thunk-router/src/Store/ConfigureStore.csrOnly.js
@@ -2,19 +2,10 @@
 // together with Uglify to strip the dev branch in prod build.
 let configureStore;
 
-
 if (process.env.NODE_ENV === 'production') {
-  // kra-mod-start
-  if (KRA.CSR) {
-    configureStore = require('./ConfigureStore.csr.prod');  // kra-mod-replace .csr
-  }
-  // kra-mod-end
+  configureStore = require('./ConfigureStore.csr.prod');  // kra-mod-replace .csr
 } else {
-  // kra-mod-start
-  if (KRA.CSR) {
-    configureStore = require('./ConfigureStore.csr.dev'); // kra-mod-replace .csr
-  }
-  // kra-mod-end
+  configureStore = require('./ConfigureStore.csr.dev'); // kra-mod-replace .csr
 }
 
 exports = module.exports = configureStore;
