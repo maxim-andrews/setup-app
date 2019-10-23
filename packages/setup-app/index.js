b780af4c8427ef0eda57b65af0b483a720c484c5
Maxim Andrews <maximandrews@users.noreply.github.com>
2019-10-23 15:09:31 -0700

    fix shell line

--- packages/setup-app/index.js
+++ packages/setup-app/index.js
@@ -1,3 +1,11 @@
+#!/usr/bin/env node
+/**
+ * Copyright (c) 2018, Maxim Andrews, maximandrews.com
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 'use strict';
 const generateNewProject = require('setup-app-templates');
 
