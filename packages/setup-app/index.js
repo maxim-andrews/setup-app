0b35b81b3c50d5b619f14acd1907b13327835d76
Maxim Andrews <maximandrews@users.noreply.github.com>
2019-10-24 12:41:04 -0700

    fix copyright notice

--- packages/setup-app/index.js
+++ packages/setup-app/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /**
- * Copyright (c) 2018, Maxim Andrews, maximandrews.com
+ * Copyright (c) 2018-present, Maxim Andrews, maximandrews.com
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
