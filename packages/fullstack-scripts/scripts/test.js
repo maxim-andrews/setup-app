5ca074b838422a6b9745672625b806926d8aa8fe
Maxim Andrews <maximandrews@users.noreply.github.com>
2019-10-25 14:41:40 -0700

    fix tests --watch and --watchAll issues

--- packages/fullstack-scripts/scripts/test.js
+++ packages/fullstack-scripts/scripts/test.js
@@ -7,6 +7,7 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
@@ -24,11 +25,27 @@ process.on('unhandledRejection', err => {
 require('../config/env');
 
 const jest = require('jest');
+const execSync = require('child_process').execSync;
+const execVC = cmd => {
+  try {
+    execSync(cmd, { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+const hasGit = () => execVC('git rev-parse --is-inside-work-tree');
+const hasMercurial = () => execVC('hg --cwd . root');
+
 let argv = process.argv.slice(2);
 
 // Watch unless on CI or in coverage mode
-if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-  argv.push('--watch');
+if (!process.env.CI
+    && argv.indexOf('--coverage') < 0
+    && argv.indexOf('--watchAll') === -1
+    && argv.indexOf('--watchAll=false') === -1) {
+  const versioned = hasGit() || hasMercurial();
+  argv.push(versioned ? '--watch' : '--watchAll');
 }
 
 const createJestConfig = require('../config/jest/jestConfig');
